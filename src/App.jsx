4460e4b1a405651325ce4acf27c40ed3ea394289
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-03-28 23:48:15 +0000

    Add routes and add content to pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home';
 import About from './pages/About';
+import Van from './pages/Van';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path="/vans" element={<Van />} />
 
             </Routes>
         </BrowserRouter>
