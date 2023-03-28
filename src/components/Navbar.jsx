1702fa4961cb39721089b881eddcc33d73df9dd6
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-03-28 23:47:41 +0000

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../Navbar.css'; // import your CSS file
+import '../index.css'; // import your CSS file
 
 
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link></li>
-                <li>
-
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
-
-        </nav>
+        <header>
+            <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+                <Link to="/about">About</Link>
+                <Link to="/vans">Vans</Link>
+            </nav>
+        </header>
     )
 }
 
