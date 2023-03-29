2a7f51d237cca620aff8f7cf87a4da724eeab52e
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-03-29 04:57:41 +0000

    fetch data from server and render van component

--- src/pages/Van.jsx
+++ src/pages/Van.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useEffect, useState, React } from 'react'
+import server from '../../server'
+import VanDetails from '../components/VanDetails'
+
 
 function Van() {
+    const [vans, setVans] = useState([])
+    useEffect(() => {
+        async function fetchData() {
+
+            const result = await fetch('/api/vans')
+                .then(res => res.json())
+                .then(data => {
+                    setVans(data.vans)
+
+                })
+
+
+        }
+        fetchData()
+
+    }, [])
+
     return (
-        <div>Van Page</div>
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vans.map(van => <VanDetails van={van} key={van.id} />)}
+            </div>
+        </div>
     )
 }
 
