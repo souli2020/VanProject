8a865f6b1c2afd9411b058b245db483263426f48
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-03-29 23:29:22 +0000

    create views for the Host page

---
+++ src/pages/Host/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+function Dashboard() {
+    return (
+        <>
+            <div>Dashboard</div>
+        </>
+
+    )
+}
+
+export default Dashboard
