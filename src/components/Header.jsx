4648f8af265fcceefaf71500b3c44bc326ea68c5
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-03-30 02:24:58 +0000

    use NavLink instead of Link and style active path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../index.css'; // import your CSS file
 
 
 
 function Header() {
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: '#161616',
+        border: "1px solid black",
+        borderRadius: '5px'
+
+    }
     return (
         <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
+            <NavLink className="site-logo" to="/">#VanLife</NavLink>
             <nav>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
-                <Link to="/host">Host</Link>
+                <NavLink
+                    to="/about"
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >About</NavLink>
+                <NavLink
+                    to="/vans"
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >Vans</NavLink>
+                <NavLink
+                    to="/host"
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >Host</NavLink>
             </nav>
         </header>
     )
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 function HostLayout() {
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "none",
+        color: '#164616',
+        border: "1px solid black",
+        borderRadius: '5px',
+
+
+    }
     return (
         <>
 
             <nav className="host-nav">
-                <Link to='/host/'>Dashboard</Link>
-                <Link to='/host/income'>Income</Link>
-                <Link to='/host/reviews'>Reviews</Link>
-                <Link to='/vans'>Vans</Link>
+                <NavLink to='/host'
+                    end
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                > Dashboard </NavLink>
+                <NavLink to='/host/income'
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >Income</NavLink>
+                <NavLink to='/host/reviews'
+                    style={({ isActive }) => isActive ? activeStyle : null}>Reviews</NavLink>
+                <NavLink to='/host/vans'
+                    style={({ isActive }) => isActive ? activeStyle : null}>Vans</NavLink>
             </nav >
             <Outlet />
         </>
