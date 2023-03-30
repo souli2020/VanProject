7f5e76508039c2a8554eb735308f6063bd643c79
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-03-30 02:21:45 +0000

    add dynamic Link

--- src/components/VanDetails.jsx
+++ src/components/VanDetails.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 function VanDetails({ ...props }) {
-    const { van, description } = props
+    const { van, description, link } = props
     return (
+        <>
+            <div id={van.id} key={van.id} className="van-tile"  >
+                <Link to={!link ? `/vans/${van.id}` : link} >
+                    <img src={van.imageUrl} />
+                    <div className="van-info">
+                        <h3>{van.name}</h3>
+                        {description && <p>{description}</p>}
+                        <p>${van.price}<span>/day</span></p>
 
-        <div id={van.id} key={van.id} className="van-tile"  >
-            <Link to={`/vans/${van.id}`} >
-                <img src={van.imageUrl} />
-                <div className="van-info">
-                    <h3>{van.name}</h3>
-                    {description && <p>{description}</p>}
-                    <p>${van.price}<span>/day</span></p>
-
-                </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            </Link>
-        </div>
+                    </div>
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                </Link>
+            </div>
+        </>
     )
 }
 
