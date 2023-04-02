b81dc39741d708b22cc90fc2f17e8326d6e50444
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-04-02 02:16:17 +0000

    Add tooltip text

--- src/pages/VanDetailsPage.jsx
+++ src/pages/VanDetailsPage.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState, React } from 'react'
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import VanDetails from '../components/VanDetails';
 
 function VanDetailsPage() {
+    const location = useLocation()
+    const type = location.state?.type || '';
+
+
     const [van, setVan] = useState({})
     const { id } = useParams();
     useEffect(() => {
@@ -24,7 +28,11 @@ function VanDetailsPage() {
             {Object.keys(van).length ?
                 (<>
                     <button className="link-button"> Rent this van </button>
-                    <VanDetails van={van} description={van.description} className="van-detail" />
+                    {type ? (
+                        <small className='link-button click-text'> click in the image to return to {type}  </small>)
+                        : <small className='link-button click-text'>click to return to all vans</small>}
+
+                    <VanDetails van={van} link={'..'} description={van.description} className="van-detail" />
 
                 </>) : <h2>Bear with us...</h2>}
         </div>
